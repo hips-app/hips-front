6e2ef24a5f5f69d00c96c4eb6cd6993919260f34
jhonatan torres <jtorresf@unal.edu.co>
2020-10-05 17:40:29 -0500

    1.0.0 create login and signup view

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Login',
+    component: Login
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
 
-export default router;
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+      <div class="popup">
+        <div class="popup-content">
+          <p>Welcome!</p>
+          <h2 class="popup-title">Sign in to <span id="hips">Hips</span></h2>
+          <form>
+            <div class="group">
+              <input type="text" required />
+              <span class="highlight"></span>
+              <span class="bar"></span>
+              <label>Email</label>
+            </div>
+
+            <div class="group">
+              <input type="password" required />
+              <span class="highlight"></span>
+              <span class="bar"></span>
+              <label>Password</label>
+            </div>
+          </form>
+          <a href="#0" class="popup-button">Sign in</a>
+          <router-link to="sign-up" id="btn-2" class="popup-button">Sign up</router-link>
+          <a href="#0" class="popup-button">Sign in with Google</a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {
+  }
+}
+</script>
