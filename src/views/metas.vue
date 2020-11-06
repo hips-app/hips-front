5e0a97ad283747dfeee1799ee1db2a1f9b904817
jsnietogi <gordiferonieto@gmail.com>
2020-11-06 09:26:40 -0500

    calorias completo

--- src/views/metas.vue
+++ src/views/metas.vue
@@ -1,44 +1,46 @@
 <template>
-  <navbar></navbar>
-  <main class="container  ">
-    <div class="popup">
-      <div class="popup-content">
-        <div class="form-group " style="">
-          <h4 class="text-light">Ingrese su meta:</h4>
-          <input
-            v-model="description"
-            type="text"
-            id="peso"
-            class="form-control"
-            style=""
-            required
-          />
-          <h4 class="text-light mt-4">
-            Ingrese su fecha de expiracion de la meta:
-          </h4>
-          <input
-            v-model="expirationDate"
-            type="date"
-            id="inputMetaDate"
-            class="form-control"
-            placeholder="cm"
-            required
-          />
-          <center>
-            <button
-              href="#0"
-              class="popup-button  mt-4"
-              v-on:click="sendGoal()"
-            >
-              enviar
-            </button>
-          </center>
+  <div>
+    <navbar></navbar>
+    <main class="container  ">
+      <div class="popup">
+        <div class="popup-content">
+          <div class="form-group " style="">
+            <h4 class="text-light">Ingrese su meta:</h4>
+            <input
+              v-model="description"
+              type="text"
+              id="peso"
+              class="form-control"
+              style=""
+              required
+            />
+            <h4 class="text-light mt-4">
+              Ingrese su fecha de expiracion de la meta:
+            </h4>
+            <input
+              v-model="expirationDate"
+              type="date"
+              id="inputMetaDate"
+              class="form-control"
+              placeholder="cm"
+              required
+            />
+            <center>
+              <button
+                href="#0"
+                class="popup-button  mt-4"
+                v-on:click="sendGoal()"
+              >
+                enviar
+              </button>
+            </center>
+          </div>
         </div>
       </div>
+    </main>
+    <div>
+      <foot></foot>
     </div>
-  </main>
-  <div>
-    <foot></foot>
   </div>
 </template>
 <script>
